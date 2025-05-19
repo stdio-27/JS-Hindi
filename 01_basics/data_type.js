e575060d46604f26e1d224bf836622f419807cd5
stdio-27 <146674947+stdio-27@users.noreply.github.com>
2025-05-19 11:07:44 +0000

    data types

---
+++ 01_basics/data_type.js
@@ -0,0 +1,6 @@
+"use strict" // using newer version of Js
+
+// alert(3+3)  can't use this as we are using node not browser
+
+console.log(typeof null) //object
+console.log(typeof undefined) // undedfined
