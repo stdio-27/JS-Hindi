8468c32d1ef7c6747b3d4c4376c9a2c55f9b2cd2
stdio-27 <146674947+stdio-27@users.noreply.github.com>
2025-05-19 10:57:00 +0000

    variables

---
+++ 01_basics/variable.js
@@ -0,0 +1,7 @@
+const account_Id=38530703804
+let Account_type="savings"
+var Account_bal=3800
+account_name="Bikash"
+let account_holder;
+
+console.table([account_Id, Account_type, Account_bal, account_name, account_holder]);
