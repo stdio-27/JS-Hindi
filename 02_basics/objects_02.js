e25b4f23e749e48320b6a87c81c31e80cc827abb
stdio-27 <146674947+stdio-27@users.noreply.github.com>
2025-05-21 04:17:14 +0000

    added some API

--- 02_basics/objects_02.js
+++ 02_basics/objects_02.js
@@ -36,4 +36,29 @@ console.log(Objects.entries(tinderuser));
 console.log(tinderuser.hasOwnProperty('loggedin'));
 
 
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
+
 
